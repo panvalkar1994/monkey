b276fd203f9751a5792582bf8130f6bd6a0d6cd4
panvalkar1994 <vaibhav.panvalkar@elastic.run>
2024-01-21 21:53:10 +0530

    integer eval

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/panvalkar1994/monkey/evaluator"
 	"github.com/panvalkar1994/monkey/lexer"
 	"github.com/panvalkar1994/monkey/parser"
 )
@@ -30,9 +31,11 @@ func Start(in io.Reader, out io.Writer) {
 			printParseErrors(out, p.Errors())
 			continue
 		}
-
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 
 	}
 }
