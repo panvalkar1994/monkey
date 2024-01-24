98d3a63b78d014a369e329c376b1a2c14afc1c27
panvalkar1994 <vaibhav.panvalkar@elastic.run>
2024-01-24 09:20:29 +0530

    ast refactoring

---
+++ ast/expression_statement.go
@@ -0,0 +1,23 @@
+package ast
+
+import (
+	"bytes"
+
+	"github.com/panvalkar1994/monkey/token"
+)
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+func (es *ExpressionStatement) String() string {
+	var out bytes.Buffer
+	if es.Expression != nil {
+		out.WriteString(es.Expression.String())
+	}
+	return out.String()
+}
