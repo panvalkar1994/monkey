86fec1780abc7cdea1308e4f02cf0eab180c3701
panvalkar1994 <vaibhav.panvalkar@elastic.run>
2024-01-24 09:24:25 +0530

    object package refactoring

---
+++ object/error.go
@@ -0,0 +1,8 @@
+package object
+
+type Error struct {
+	Message string
+}
+
+func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
+func (e *Error) Type() ObjectType { return ERROR_OBJ }
---
+++ object/null.go
@@ -0,0 +1,7 @@
+package object
+
+type Null struct{}
+
+func (n *Null) Inspect() string { return "null" }
+
+func (n *Null) Type() ObjectType { return NULL_OBJ }
